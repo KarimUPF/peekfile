7cbc37f2db96103ae76028691a567f29cd158559
KarimUPF <karim.hamed01@estudiant.upf.edu>
2024-10-29 16:03:51 +0100

    Homework Day 10

--- peekfile.sh
+++ peekfile.sh
@@ -2,6 +2,14 @@
 file=$1
 lines=$2
 
-head -n "$lines" "$file"
-echo "..."
-tail -n "$lines" "$file"
+if [[ -z $lines ]]; then
+  lines=3
+fi
+
+if [[ $(wc -l < $file) -le 2*$lines ]]; then
+  cat $file
+else
+  head -n "$lines" "$file"
+  echo "..."
+  tail -n "$lines" "$file"
+fi
